6f79bf53c266e9ecf4b72831db6c0c6d6eceb552
Nikhil Shirke <Nikhil Shirke@LAPTOP-ISMUH2LS>
2026-06-12 19:46:18 +0530

    Initial Spring Boot Website

---
+++ lilacsip/src/main/java/com/lilacsip/com/lilacsip/LilacsipApplication.java
@@ -0,0 +1,13 @@
+package com.lilacsip.com.lilacsip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilacsipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilacsipApplication.class, args);
+	}
+
+}
